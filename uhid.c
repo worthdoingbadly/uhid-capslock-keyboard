73c4e05b17fd7da3df6657c0f1c82e2c86dc69b3
Zhuowei Zhang <zhuowei@worthdoingbadly.com>
2025-05-17 18:53:40 -0400

    demo of HID_DG_CONTACTMAX with logical_maximum=0

--- uhid.c
+++ uhid.c
@@ -2,6 +2,7 @@
 #include <linux/uhid.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -9,7 +10,7 @@ const uint8_t kDescriptor[] = {
 #include "descriptor.h"
 };
 
-int main() {
+int main(int argc, char** argv) {
   int fd = open("/dev/uhid", O_RDWR);
   {
     struct uhid_event event = {
@@ -21,8 +22,8 @@ int main() {
                 .uniq = {0},
                 .rd_size = sizeof(kDescriptor),
                 .bus = BUS_USB,
-                .vendor = 0x045e,
-                .product = 0x076c,
+                .vendor = 0x0596,
+                .product = 0x0500,  // USB_DEVICE_ID_3M1968
                 .version = 0x1234,
                 .country = 0,
             },
@@ -41,11 +42,36 @@ int main() {
       case UHID_START:
       case UHID_OPEN:
         break;
+      case UHID_STOP:
+        fprintf(stderr, "stop\n");
+        break;
+      case UHID_CLOSE:
+        fprintf(stderr, "close\n");
+        return 1;
       case UHID_OUTPUT:
         fprintf(stderr, "uhid output! size=%d rtype=%d output=%x %x %x\n",
                 in_event.u.output.size, in_event.u.output.rtype,
                 in_event.u.output.data[0], in_event.u.output.data[1],
                 in_event.u.output.data[2]);
+        break;
+      case UHID_GET_REPORT: {
+        fprintf(stderr, "get report id=%x rnum=%x rtype=%x\n",
+                in_event.u.get_report.id, in_event.u.get_report.rnum,
+                in_event.u.get_report.rtype);
+        struct uhid_event event = {.type = UHID_GET_REPORT_REPLY,
+                                   .u.get_report_reply = {
+                                       .id = in_event.u.get_report.id,
+                                       .err = 0,
+                                       .size = 1,
+                                   }};
+        uint8_t outdata = argc == 2 ? atoi(argv[1]) : 0;
+        fprintf(stderr, "returning data=%x\n", outdata);
+        event.u.get_report_reply.data[0] = outdata;
+        write(fd, &event, sizeof(event));
+        break;
+      }
+      case UHID_SET_REPORT:
+        fprintf(stderr, "set report\n");
         break;
       default:
         printf("unknown event %d\n", in_event.type);
